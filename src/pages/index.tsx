253d09a8a833db2c354707dc582cf8be4b9961a2
Bruno Leite <bruunopereira19@gmail.com>
2023-05-11 00:13:35 -0300

    :memo: Initial project

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { IconAngle } from "@tabler/icons-react";
+
+export default function Home() {
+  return (
+    <div
+      className={`flex justify-center items-center bg-black text-white text-6xl h-screen
+    `}
+    >
+      <IconAngle size={100} strokeWidth={0.5}/>
+      Fundamentos
+    </div>
+  );
+}
